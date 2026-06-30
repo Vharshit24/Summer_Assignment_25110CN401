0ae26bc496284e74f6d0cd18eb30280cff2b0ae5
HARSHIT VERMA <vharshit0224@gmail.com>
2026-06-30 21:02:02 +0530

    Updated

--- Day-03/Q10.java
+++ Day-03/Q10.java
@@ -4,22 +4,29 @@ import java.util.Scanner;
 
 public class Q10 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)){
         System.out.println("Enter Number");
         int originalNum = sc.nextInt();
+         if (originalNum<= 1) {
+            System.out.println("Enter number greater than 1");
+            return;
+        }
         System.out.println("The prime numbers in given range: ");
-        for (int num = 1; num <= originalNum; num++) {
-            int i;
-            for (i = 2; i < num; i++) {
-                if (num % i == 0) {
+        for (int num = 2; num <= originalNum; num++) {
+             boolean isPrime= true;
 
+             // Only checks up to the square root of num
+             
+            for (int i = 2; i*i <= num; i++) {
+                if (num % i == 0) {
+                    isPrime= false;
                     break;
                 }
             }
-            if (i == num) {
+            if (isPrime) {
                 System.out.print(num + " ");
             }
         }
-        sc.close();
+    }
     }
 }
